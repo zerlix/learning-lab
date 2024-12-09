5db09fc606f9a90b236db58746426bf57d326246
zerlix <t.hemm@t-online.de>
2024-12-09 20:39:45 +0100

    ...

--- dbewings/resources/scripts/flugsuche.js
+++ dbewings/resources/scripts/flugsuche.js
@@ -1,4 +1,4 @@
-const endpoint =  "https://storage01.dbe.academy/fswd/travel-api.php?start=gibtsnicht&ziel=nirgendwo&datum=05.12.2024";
+const endpoint = "https://storage01.dbe.academy/fswd/travel-api.php?start=gibtsnicht&ziel=nirgendwo&datum=05.12.2024";
 /*
     Daten per Fetch holen und im Main-Bereich anzeigen
 */
@@ -9,26 +9,27 @@ document.querySelector("#form-fluege")?.addEventListener("submit", async functio
   const mainContent = document.querySelector("main");
 
   if (overlay && mainContent) {
-      // Overlay anzeigen
-      overlay.classList.remove("hidden");
+    // Overlay anzeigen
+    overlay.classList.remove("hidden");
 
-      try {
-          // Fetch-Request 
-          const response = await fetch(endpoint);
-          if (!response.ok) {
-              throw new Error(`Fehler: ${response.status} - ${response.statusText}`);
-          }
+    let data;
 
-          const data = await response.json(); // JSON-Daten parsen
-
-          // JSON-Daten im Main-Bereich anzeigen
-          mainContent.textContent = JSON.stringify(data, null, 2); // Formatierte JSON-Ausgabe
-      } catch (error) {
-          // Fehlerbehandlung und Fehlermeldung im Main-Bereich
-          mainContent.textContent = `Fehler beim Laden der Daten: ${error.message}`;
-      } finally {
-          // Overlay nach kurzer Zeit ausblenden
-          setTimeout(() => overlay.classList.add("hidden"), 1000);
+    // Fetch-Request 
+    try {
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error(`Fehler: ${response.status} - ${response.statusText}`);
       }
+      
+      data = await response.json(); // JSON-Daten parsen
+
+    } catch (error) {
+      mainContent.textContent = `Fehler beim Laden der Daten: ${error.message}`;
+    } finally {
+      // Overlay spielerei - Timer extra eingebaut wird nach kurzer Zeit ausblenden
+      setTimeout(() => overlay.classList.add("hidden"), 1000);
+      // JSON-Daten im Main-Bereich anzeigen
+      mainContent.textContent = JSON.stringify(data, null, 2); // Formatierte JSON-Ausgabe
+    }
   }
 });
