38b0422137a8c18b4c307580c956e98402913338
zerlix <t.hemm@t-online.de>
2024-12-18 11:43:30 +0100

    new file:   react/jsx-demo/src/Navbar.js

---
+++ react/jsx-demo/src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">MyApp</h1>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
