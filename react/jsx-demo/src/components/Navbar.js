a4e1a61105b91b0d6f3d7c28cb9b2c28333b5af2
Zerlix <t.hemm@t-onkine.de>
2024-12-19 10:37:41 +0100

    new file:   react/jsx-demo/src/components/Button.js

--- react/jsx-demo/src/components/Navbar.js
+++ react/jsx-demo/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../styles/Navbar.css'; // Importpfad angepasst
+import { Button } from '@mui/material';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="navbar-logo">MyApp</h1>
       <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Button href="#home" variant="outlined">Home</Button></li>
+        <li><Button href="#about" variant="outlined">About</Button></li>
+        <li><Button href="#services" variant="outlined">Services</Button></li>
+        <li><Button href="#contact" variant="outlined">Kontakt</Button></li>
       </ul>
     </nav>
   );
--- react/jsx-demo/src/index.js
+++ react/jsx-demo/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/Navbar'; // Importpfad angepasst
+import Navbar from './components/Navbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Navbar />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
